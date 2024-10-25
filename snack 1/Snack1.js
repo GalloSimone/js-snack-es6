36248d9a5ca37c20335d5525d78edea60d16126c
GalloSimone <gallosimonecz@gmail.com>
2024-10-25 15:42:16 +0200

    objects

---
+++ snack 1/Snack1.js
@@ -0,0 +1,40 @@
+/**Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.*/
+
+const bicycleOne={
+name:  'CanyonUltimate', 
+weight:{
+    value:'6,8',
+    unit:'kg'
+}
+}
+const bicycleTwo={
+    name:  ' Specialized', 
+    weight:{
+        value:'6,7',
+        unit:'kg'
+    }
+}
+const bicycleThree={
+    name:  'TrekEmonda', 
+    weight:{
+        value:'6,5',
+        unit:'kg'
+    }
+}
+const bicycleFour={
+    name:  'Pinarello', 
+    weight:{
+        value:'6,9',
+        unit:'kg'
+    }
+}
+const bicycleFive={
+    name:  'Bianchi', 
+    weight:{
+        value:'7,1',
+        unit:'kg'
+    }
+}
